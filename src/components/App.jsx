30c0a2c019f4bd5d722f57aa1e9a13c48783510e
Volin13 <110535249+Volin13@users.noreply.github.com>
2022-12-27 22:19:02 +0200

    commit message

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
+import { useEffect, useState } from 'react';
+import { Section, Filter, Contacts, NewContactForm, Container } from './index';
+import { nanoid } from 'nanoid';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const savedData = localStorage.getItem('contacts');
+    const parsedData = JSON.parse(savedData);
+    return parsedData ?? [];
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const seveData = JSON.stringify(contacts);
+    return localStorage.setItem('contacts', seveData);
+  }, [contacts]);
+
+  const addContact = ({ name, number }) => {
+    if (
+      contacts.some(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert('Contact already exists');
+    } else {
+      setContacts(phones => [...phones, { id: nanoid(), name, number }]);
+    }
+  };
+
+  const formatedFilter = filter => {
+    setFilter(filter.toLowerCase().trim());
+  };
+
+  const filterContacts = () => {
+    return contacts.filter(({ name }) => name.toLowerCase().includes(filter));
+  };
+
+  const removeContact = idToRemove => {
+    setContacts(phones => phones.filter(({ id }) => id !== idToRemove));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container title="Phone book">
+      <Section title="Add new contact" variant="h3">
+        <NewContactForm addContact={addContact} />
+      </Section>
+      <Section title="Your contacts" variant="h3">
+        <Contacts contactsList={filterContacts()} removeContact={removeContact}>
+          <Filter setFilter={formatedFilter} />
+        </Contacts>
+      </Section>
+    </Container>
   );
 };
